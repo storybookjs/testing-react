212aa9cfd67a415a0dd8e3bf2982dae377cabd0d
Yann Braga <yannbf@gmail.com>
2021-08-20 17:41:04 +0200

    feat: add error handling in global render function

--- src/utils.tsx
+++ src/utils.tsx
@@ -4,6 +4,13 @@ import type { FunctionComponent } from 'react';
 import type { Story } from '@storybook/react';
 
 export const globalRender: Story = (args, { parameters }) => {
+  if (!parameters.component) {
+    throw new Error(`
+      Could not render story due to missing 'component' property in Meta.
+      Please refer to https://github.com/storybookjs/testing-react#csf3
+    `);
+  }
+
   const Component = parameters.component as FunctionComponent;
   return <Component {...args} />;
 };
