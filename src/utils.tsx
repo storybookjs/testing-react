732e18693576c902f42f603527752bacde476700
Yann Braga <yannbf@gmail.com>
2022-01-13 16:44:38 +0100

    chore: remove unused code

--- src/utils.tsx
+++ src/utils.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 
 import type { ArgsStoryFn } from '@storybook/csf';
 import type { ReactFramework } from '@storybook/react';
-import type { TestingStory } from './types';
-
-const invalidStoryTypes = new Set(['string', 'number', 'boolean', 'symbol']);
-
-export const isInvalidStory = (story?: any) => (!story || Array.isArray(story) || invalidStoryTypes.has(typeof story))
 
 export const globalRender: ArgsStoryFn<ReactFramework> = (args, context) => {
   const { id, component: Component } = context;
@@ -24,16 +19,4 @@ type Entries<T> = {
 }[keyof T];
 export function objectEntries<T extends object>(t: T): Entries<T>[] {
   return Object.entries(t) as any;
-}
-
-export const getStoryName = (story: TestingStory) => {
-  if (story.storyName) {
-    return story.storyName
-  }
-
-  if (typeof story !== 'function' && story.name) {
-    return story.name
-  }
-
-  return undefined
 }
